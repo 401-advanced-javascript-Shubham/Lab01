41dff651e3512dcf9156d6b4b380e7d877c21d8e
smajumdar22 <53415839+smajumdar22@users.noreply.github.com>
2020-01-06 14:59:39 -0800

    Update validator.test.js

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -28,13 +28,59 @@ describe('validator module performs basic validation of', () => {
 
   // TODO: Make this series of tests less repetitive ... DRY it out
 
-  it('strings', () => {
-    let str = 'yes';
+   let str = 'yes';
     let num = 1;
     let arr = ['a'];
     let obj = {x:'y'};
     let func = () => {};
     let bool = false;
+  
+  it('strings', () => {
+    expect(validator.isString(str)).toBeTruthy();
+    expect(validator.isString(num)).toBeFalsy();
+    expect(validator.isString(arr)).toBeFalsy();
+    expect(validator.isString(obj)).toBeFalsy();
+    expect(validator.isString(func)).toBeFalsy();
+    expect(validator.isString(bool)).toBeFalsy();
+  });
+  
+   it('number', () => {
+    expect(validator.isString(str)).toBeTruthy();
+    expect(validator.isString(num)).toBeFalsy();
+    expect(validator.isString(arr)).toBeFalsy();
+    expect(validator.isString(obj)).toBeFalsy();
+    expect(validator.isString(func)).toBeFalsy();
+    expect(validator.isString(bool)).toBeFalsy();
+  });
+  
+    it('array', () => {
+    expect(validator.isString(str)).toBeTruthy();
+    expect(validator.isString(num)).toBeFalsy();
+    expect(validator.isString(arr)).toBeFalsy();
+    expect(validator.isString(obj)).toBeFalsy();
+    expect(validator.isString(func)).toBeFalsy();
+    expect(validator.isString(bool)).toBeFalsy();
+  });
+  
+    it('object', () => {
+    expect(validator.isString(str)).toBeTruthy();
+    expect(validator.isString(num)).toBeFalsy();
+    expect(validator.isString(arr)).toBeFalsy();
+    expect(validator.isString(obj)).toBeFalsy();
+    expect(validator.isString(func)).toBeFalsy();
+    expect(validator.isString(bool)).toBeFalsy();
+  });
+  
+    it('function', () => {
+    expect(validator.isString(str)).toBeTruthy();
+    expect(validator.isString(num)).toBeFalsy();
+    expect(validator.isString(arr)).toBeFalsy();
+    expect(validator.isString(obj)).toBeFalsy();
+    expect(validator.isString(func)).toBeFalsy();
+    expect(validator.isString(bool)).toBeFalsy();
+  });
+  
+    it('boolean', () => {
     expect(validator.isString(str)).toBeTruthy();
     expect(validator.isString(num)).toBeFalsy();
     expect(validator.isString(arr)).toBeFalsy();
@@ -43,74 +89,7 @@ describe('validator module performs basic validation of', () => {
     expect(validator.isString(bool)).toBeFalsy();
   });
 
-  let intializetTest = {
-    'string': str,
-    'number': num,
-    'array': arr,
-    'object': obj,
-    'function': func,
-    'boolean': bool
-  }
-  it('numbers', () => {
-    for(let input in intializetTest){
-      if(input === 'number'){
-        expect(validator.isNumber(intializetTest[input])).toBeTruthy();
-      }else{
-        expect(validator.isNumber(intializetTest[input])).toBeFalsy();
-      }
-    }
-  });
-
- it('string', () => {
-    for(let input in intializetTest){
-      if(input === 'string'){
-        expect(validator.isString(intializetTest[input])).toBeTruthy();
-      }else{
-        expect(validator.isString(intializetTest[input])).toBeFalsy();
-      }
-    }
-  });
-
-  it('objects', () => {
-    for(let input in intializetTest){
-      if(input === 'object'){
-        expect(validator.isObject(intializetTest[input])).toBeTruthy();
-      }else{
-        expect(validator.isObject(intializetTest[input])).toBeFalsy();
-      }
-    }
-  });
-
-  it('booleans', () => {
-    for(let input in intializetTest){
-      if(input === 'boolean'){
-        expect(validator.isBoolean(intializetTest[input])).toBeTruthy();
-      }else{
-        expect(validator.isBoolean(intializetTest[input])).toBeFalsy();
-      }
-    }
-  });
-
-  it('functions', () => {
-    for(let input in intializetTest){
-      if(input === 'function'){
-        expect(validator.isFunction(intializetTest[input])).toBeTruthy();
-      }else{
-        expect(validator.isFunction(intializetTest[input])).toBeFalsy();
-      }
-    }
-  });
-
-  it('arrays', () => {
-    for(let input in intializetTest){
-      if(input === 'array'){
-        expect(validator.isArray(intializetTest[input])).toBeTruthy();
-      }else{
-        expect(validator.isArray(intializetTest[input])).toBeFalsy();
-      }
-    }
-  });
-
+  
 
 
 describe('validator module performs complex validations', () => {
